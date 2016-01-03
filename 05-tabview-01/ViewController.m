1d1f68abb8f2ad7ce956d7ed0d082eb96fec9c6c
zhanglu1988em <zhanglu1988em163.com@zhanglu1988emdemacbook-pro.local>
2016-01-03 23:03:05 +0800

    tableview 简单实用 1，分组标题和页脚

--- 05-tabview-01/ViewController.m
+++ 05-tabview-01/ViewController.m
@@ -8,20 +8,49 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate,UITableViewDataSource>
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return section == 0 ? 8 : 18;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+// Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+// Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *id = @"cell";
+    
+    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:id];
+    
+    [cell.textLabel setText:[NSString stringWithFormat:@"第%02d学员 - %02d",indexPath.section,indexPath.row]];
+    return cell;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 2;
+}
+
+// 添加分组标题
+
+-(NSString*) tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    return [NSString stringWithFormat:@"第 %02d 期",section];
+}
+
+//添加分组页脚
+-(NSString*) tableView:(UITableView *)tableView titleForFooterInSection:(NSInteger)section
+{
+    if (section == 0) {
+        return @"太牛叉了";
+    }
+    return @"相当牛叉";
 }
 
 @end
